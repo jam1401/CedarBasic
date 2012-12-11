070e7902c0a4638598b76037c1cc10b005a90bdc
Jeff Morgan <jam1401@gmail.com>
2012-12-11 09:26:24 -0800

    Added CEDAR to project the easy way :-)

---
+++ Specs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Specs
+//
+//  Created by Jeffrey Morgan on 12/11/12.
+//  Copyright (c) 2012 Jeffrey Morgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Cedar-iOS/Cedar-iOS.h>
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
+  }
+}
